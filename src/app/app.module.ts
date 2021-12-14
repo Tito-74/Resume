0364ae95840d0c31762a068b36c9f3b8b95c49c5
Tito-74 <kipkirui133@gmail.com>
2021-12-14 11:46:31 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExperienceComponent } from './commponents/experience/experience.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExperienceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
